c031bd0105744180ee5734e0260abc2c23b2b8a7
AdamBcit <132965019+AdamBcit@users.noreply.github.com>
2026-03-09 11:24:28 -0700

    dev/assimp-6.0.4

---
+++ Dev/assimp-6.0.4/build/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/adamd/Downloads/assimp-6.0.4/assimp-6.0.4/contrib/zlib
+# Build directory: C:/Users/adamd/Downloads/assimp-6.0.4/assimp-6.0.4/build/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
