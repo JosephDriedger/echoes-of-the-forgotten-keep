be87c88e3771b13ca2797c811adb290371b55459
AdamBcit <132965019+AdamBcit@users.noreply.github.com>
2026-03-11 13:52:10 -0700

    Update CameraSystem.h

--- src/ecs/system/CameraSystem.h
+++ src/ecs/system/CameraSystem.h
@@ -24,31 +24,21 @@ class CameraSystem {
 
         if (!playerEntity) return;
 
-        auto& playerTransform = playerEntity->getComponent<Transform>();
+        auto& playerTransform = playerEntity->getComponent<Transform3D>();
 
         for (auto& e : entities) {
-            if (e->hasComponent<Camera>()) {
+            if (e->hasComponent<Camera3D>()) {
 
-                auto& cam = e->getComponent<Camera>();
+                auto& cam = e->getComponent<Camera3D>();
+                auto& transform = e->getComponent<Transform3D>();
 
                 // this position the camera so the player is at the center of its view
-                cam.view.x = playerTransform.position.x - cam.view.w / 2;
-                cam.view.y = playerTransform.position.y - cam.view.h / 2;
+                transform.position.x = playerTransform.position.x -  0;
+                transform.position.z = playerTransform.position.z +  5;
 
                 // the camera is position itself so player is centered, but the player could walk off the screen
                 // so clamp the camera so it stays within the window
-                if (cam.view.x < 0) {
-                    cam.view.x = 0;
-                }
-                if (cam.view.y < 0) {
-                    cam.view.y = 0;
-                }
-                if (cam.view.x > cam.worldWidth - cam.view.w) {
-                    cam.view.x = cam.worldWidth - cam.view.w;
-                }
-                if (cam.view.y > cam.worldHeight - cam.view.h) {
-                    cam.view.y = cam.worldHeight - cam.view.h;
-                }
+
 
 
             }
