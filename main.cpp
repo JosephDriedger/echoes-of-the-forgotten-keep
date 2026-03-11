72515d46bfa34b9e9d7cbd835f4edafa5abeb2f4
AdamBcit <132965019+AdamBcit@users.noreply.github.com>
2026-03-11 13:30:51 -0700

    update

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by adamd on 2026-03-11.
+//
+
+#include "game/GameApp.h"
+
+#include <exception>
+#include <iostream>
+
+int main()
+{
+    try
+    {
+        game::GameApp app;
+        return app.Run();
+    }
+    catch (const std::exception& exception)
+    {
+        std::cerr << "[EFK] Unhandled exception: " << exception.what() << '\n';
+    }
+    catch (...)
+    {
+        std::cerr << "[EFK] Unhandled unknown exception.\n";
+    }
+
+    return -1;
+}
